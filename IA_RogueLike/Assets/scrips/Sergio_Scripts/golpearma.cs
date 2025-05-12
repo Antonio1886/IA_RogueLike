d1b00f70b39d43cfe8613529403c2134549a5e60
Sergio Salazar <sssr308a@gmail.com>
2025-05-12 17:22:48 -0600

    animaciones espada

--- IA_RogueLike/Assets/scrips/Sergio_Scripts/golpearma.cs
+++ IA_RogueLike/Assets/scrips/Sergio_Scripts/golpearma.cs
@@ -3,17 +3,22 @@ using UnityEngine;
 public class golpearma : MonoBehaviour
 {
     private Animator animator;
+    public float cooldownTime = 1.0f; // Tiempo de cooldown en segundos
+    private float nextGolpeTime = 0f; // Tiempo en que se podrá ejecutar el próximo golpe
+
     void Start()
     {
         animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        // Verifica si se presionó el botón y si ha pasado el tiempo de cooldown
+        if (Input.GetMouseButtonDown(0) && Time.time >= nextGolpeTime)
         {
             animator.SetTrigger("Golpear");
+            // Establece el próximo tiempo disponible para golpear
+            nextGolpeTime = Time.time + cooldownTime;
         }
     }
 }
