2c4ec7e5a9de3a4ee581d6b3e0ced81e8463bd01
Sergio Salazar <sssr308a@gmail.com>
2025-05-26 12:47:03 -0600

    sistema leveleo
    eso, cambia el arma dependiendo del nivel del jugador y UI base para su vida

---
+++ IA_RogueLike/Assets/scrips/Sergio_Scripts/ExperienceManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ExperienceManager : MonoBehaviour
+{
+    [Header("Experience")]
+    [SerializeField] AnimationCurve experienceCurve;
+
+    public static int CurrentLevel { get; private set; } // ← Nivel accesible
+
+    int totalExperience;
+    int previousLevelsExperience, nextLevelsExperience;
+
+    [Header("Interface")]
+    [SerializeField] TextMeshProUGUI levelText;
+    [SerializeField] TextMeshProUGUI experienceText;
+    [SerializeField] Image experienceFill;
+
+    void Start()
+    {
+        UpdateLevel();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            AddExperience(100);
+        }
+    }
+
+    public void AddExperience(int amount)
+    {
+        totalExperience += amount;
+        CheckForLevelUp();
+        UpdateInterface();
+    }
+
+    void CheckForLevelUp()
+    {
+        if (totalExperience >= nextLevelsExperience)
+        {
+            CurrentLevel++;
+            UpdateLevel();
+        }
+    }
+
+    void UpdateLevel()
+    {
+        previousLevelsExperience = (int)experienceCurve.Evaluate(CurrentLevel);
+        nextLevelsExperience = (int)experienceCurve.Evaluate(CurrentLevel + 1);
+        UpdateInterface();
+    }
+
+    void UpdateInterface()
+    {
+        int start = totalExperience - previousLevelsExperience;
+        int end = nextLevelsExperience - previousLevelsExperience;
+
+        levelText.text = CurrentLevel.ToString();
+        experienceText.text = start + " exp / " + end + " exp";
+        experienceFill.fillAmount = (float)start / (float)end;
+    }
+}
+
--- IA_RogueLike/Assets/scrips/Sergio_Scripts/golpearma.cs
+++ IA_RogueLike/Assets/scrips/Sergio_Scripts/golpearma.cs
@@ -2,22 +2,69 @@ using UnityEngine;
 
 public class golpearma : MonoBehaviour
 {
+    [Header("Armas")]
+    public Sprite armalvl0;
+    public Sprite armalvl1;
+    public Sprite armalvl2;
+    public Sprite armalvl3;
+    public Sprite armalvl4;
+    public Sprite armalvl5;
+    public Sprite armalvl6;
+    public Sprite armalvl7;
+
+    [Header("Sprite Renderer del arma")]
+    public SpriteRenderer armaRenderer;
+
+    [Header("Animator")]
     private Animator animator;
-    public float cooldownTime = 1.0f; // Tiempo de cooldown en segundos
-    private float nextGolpeTime = 0f; // Tiempo en que se podrá ejecutar el próximo golpe
+
+    public float cooldownTime = 1.0f;
+    private float nextGolpeTime = 0f;
+
+    private int lastLevel = -1;
 
     void Start()
     {
         animator = GetComponent<Animator>();
+        UpdateWeaponSprite();
     }
 
     void Update()
     {
-        // Verifica si se presionó el botón y si ha pasado el tiempo de cooldown
+        if (ExperienceManager.CurrentLevel != lastLevel)
+        {
+            UpdateWeaponSprite();
+        }
+
         if (Time.time >= nextGolpeTime)
         {
             animator.SetTrigger("Golpear");
             nextGolpeTime = Time.time + cooldownTime;
+        }
+    }
+
+    void UpdateWeaponSprite()
+    {
+        lastLevel = ExperienceManager.CurrentLevel;
+
+        Sprite nuevoSprite = armalvl0;
+
+        switch (lastLevel)
+        {
+            case 0: nuevoSprite = armalvl0; break;
+            case 1: nuevoSprite = armalvl1; break;
+            case 2: nuevoSprite = armalvl2; break;
+            case 3: nuevoSprite = armalvl3; break;
+            case 4: nuevoSprite = armalvl4; break;
+            case 5: nuevoSprite = armalvl5; break;
+            case 6: nuevoSprite = armalvl6; break;
+            case 7: nuevoSprite = armalvl7; break;
+            default: nuevoSprite = armalvl7; break;
+        }
+
+        if (armaRenderer != null)
+        {
+            armaRenderer.sprite = nuevoSprite;
         }
     }
 }
